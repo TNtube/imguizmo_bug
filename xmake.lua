7c9e6027c9cb5fe9c8d78be86b94625da01dcb17
Stanis Kapusta <51389578+TNtube@users.noreply.github.com>
2022-08-23 00:02:11 +0200

    Update project

--- xmake.lua
+++ xmake.lua
@@ -1,13 +1,12 @@
 add_rules("mode.debug", "mode.release")
-add_rules("plugin.vsxmake.autoupdate")
 set_languages("cxx20")
 set_optimize("fastest")
 
 
-add_requires("glfw 3.3.6")
+add_requires("glfw 3.3.8")
+add_requires("glad v0.1.36")
 add_requires("imgui v1.83-docking", {configs = {glfw_opengl3 = true}})
 add_requires("glm 0.9.9+8")
-add_requires("glad v0.1.36")
 add_requires("imguizmo 1.83")
 add_requireconfs("imguizmo.imgui", {override = true, version = "v1.83-docking", configs = {glfw_opengl3 = true}}) -- config sub imgui module
 
@@ -15,4 +14,10 @@ target("App")
     set_kind("binary")
     set_runtimes("MT")
     add_files("src/**.cpp")
-    add_packages("glfw", "imgui", "glm", "imguizmo", "glad")
+    add_headerfiles("src/**.h")
+    add_includedirs("include")
+    add_packages("glfw")
+    add_packages("glad")
+    add_packages("imgui", { public = true })
+    add_packages("glm", { public = true })
+    add_packages("imguizmo")
